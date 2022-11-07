b8f7794e2aa74b9bfeb56e3c2732fa79177396d7
Dylan Wootton <wootton.dylan@gmail.com>
2022-11-07 10:41:55 -0500

    add exports

--- altair_express/__init__.py
+++ altair_express/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """Dylan Wootton"""
 __email__ = 'dwootton@mit.edu'
 __version__ = '0.1.1'
+
+from .specs.compound_plots.joint_plot import *
+from .specs.compound_plots.pair_plot import *
+from .specs.distributional import *
+from .specs.relational import *
