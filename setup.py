cf810f003982e9eabf4f2b905fcb20aa0b73a2a4
Dylan Wootton <wootton.dylan@gmail.com>
2023-01-31 16:10:28 -0300

    fixed

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme from .relational.joint_plot+ '\n\n' + history,
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='altair_express',
     name='altair_express',
