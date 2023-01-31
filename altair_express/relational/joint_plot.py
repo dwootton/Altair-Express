ac2469ad5023652ec392fe978730873cccfe07d9
Dylan Wootton <wootton.dylan@gmail.com>
2023-01-31 16:09:17 -0300

    fixed joint

--- altair_express/relational/joint_plot.py
+++ altair_express/relational/joint_plot.py
@@ -4,7 +4,7 @@ import altair as alt
 from ..distributional import hist
 from .relational import scatterplot
 
-def jointplot(data=None,x=None, y=None):
+def joint_plot(data=None,x=None, y=None):
   
   top = hist(data=data,x=x,width=200,height=50,xAxis=None,yAxis=None)
   right = hist(data=data,y=y,width=50,height=200,xAxis=None,yAxis=None)
