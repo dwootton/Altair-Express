890e72b7d0a5f3cc1d260a8b2dbb4792686fa0c2
Dylan Wootton <wootton.dylan@gmail.com>
2023-02-16 12:28:50 -0500

    Bump version: 0.1.24 → 0.1.25

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dwootton/altair_express',
-    version='0.1.24',
+    version='0.1.25',
     zip_safe=False,
 )
