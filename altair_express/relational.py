258f85335282c143b53c1c6c2500e491609686b3
Dylan Wootton <wootton.dylan@gmail.com>
2023-02-16 12:27:55 -0500

    fixed lineplot fill

--- altair_express/relational.py
+++ altair_express/relational.py
@@ -57,13 +57,11 @@ def barplot(data=None, x=None, y=None,y_axis=alt.Axis(),x_axis=alt.Axis(),color=
 def lineplot(data=None, x=None, y=None,y_axis=alt.Axis(),x_axis=alt.Axis(),color=None,effects=None,width=200,height=200):
   data, x, y = create_dataframe(data=data,x=x,y=y)
 
-  fill = 'steelblue'
-  
-  chart = alt.Chart(data).mark_line(fill=fill).encode(
+  chart = alt.Chart(data).mark_line().encode(
     alt.X(x, scale=alt.Scale(zero=False),axis=x_axis),
     alt.Y(y, scale=alt.Scale(zero=False),axis=y_axis),
   )
-
+  
   if color:
     if color not in data.columns:
         fill = color
